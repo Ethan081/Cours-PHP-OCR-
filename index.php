1d2d3b7cef23a92da088ac4db695637c8e22bde0
zemac <etienne.ziegelmeyer@lapiscine.pro>
2020-01-20 10:56:49 +0100

    add balise form & input

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
   <script src="script.js"></script>
 </head>
 <body>
-    <a href="bonjour.php?nom=Dupont&amp;prenom=Jean&amp;repete=4">Dis-moi bonjour !</a>  <a href="formulaire.php">Formulaire!</a>
+    <a href="bonjour.php?nom=Dupont&amp;prenom=Jean&amp;repete=4">Dis-moi bonjour !</a><br/>  
+    <a href="formulaire.php">Formulaire!</a><br />
+    <a href="upload.php">Upload!</a><br />
 </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+    <form action="cible_envoi.php" method="post" enctype="multipart/form-data">
+        <p>
+            Formulaire d envoie de fichier:<br />
+            <input type="file" name="monfichier"><br />
+            <input type="submit" value="Envoyer mon fichier">
+        </p>
+    </form>
+ 
+
+</body>
+</html>
