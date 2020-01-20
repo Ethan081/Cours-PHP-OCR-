cf78032e3d0a32a57d490f86ac10779dc53b4690
zemac <etienne.ziegelmeyer@lapiscine.pro>
2020-01-20 13:14:09 +0100

    create file for tp code nasa

--- cible_envoi.php
+++ cible_envoi.php
@@ -8,21 +8,23 @@
 </head>
 <body>
   <?php 
+  // $_FILES-> est un array
     if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0) 
     {
 
       if ($_FILES['monfichier']['size'] <= 1000000) 
       {
-
+          // pathinfo-> La fonctionpathinforenvoie un array contenant entre autres l'extension du fichier dans$infosfichier['extension']
           $infos_fichier = pathinfo($_FILES['monfichier']['name']);
           $extention_upload = $infos_fichier['extension'];
           $extention_autoriser = array('jpg', 'jpeg', 'png', 'gif');
-
+          // in_array()->vérifier si l'extension récupérée fait bien partie des extensions autorisées 
           if (in_array($extention_upload, $extention_autoriser)) 
           {
             $tmp_name = $_FILES['monfichier']['tmp_name'];
             
             $name = basename($_FILES['monfichier']['name']);
+            // move_uploaded_file()->Si tout est bon, on accepte le fichier
             move_uploaded_file($tmp_name, 'upload/'. $name);
             echo "L'envoie a bien ete effectué!<br />";
             echo '<img src="upload/'.$name.'" alt="" />';
